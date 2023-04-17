2132104294a76730d1564f3b7f2d5b75a74f2263
Deepak Rana <dsr4hsp@gmail.com>
2023-04-17 11:57:12 +1000

    modified:   terraform.tfvars 	modified:   variables.tf

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,5 @@
-region = "ap-southeast-2"
+region            = "ap-southeast-2"
+instance_type_web = "t2.micro"
+instance_type_app = "t2.medium"
+webserver_count   = 2
+appserver_count   = 2
--- variables.tf
+++ variables.tf
@@ -7,3 +7,12 @@ variable "instance_type_web" {
 
 variable "instance_type_app" {
 }
+
+
+variable "webserver_count" {
+  type    = number
+}
+
+variable "appserver_count" {
+  type    = number
+}
