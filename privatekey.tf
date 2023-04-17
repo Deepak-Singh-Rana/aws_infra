cbe9ad425d3ef0372de45ea0c9e05d12fa008b70
Deepak Rana <dsr4hsp@gmail.com>
2023-04-17 21:39:50 +1000

    modified:   privatekey.tf

--- privatekey.tf
+++ privatekey.tf
@@ -5,9 +5,9 @@ resource "tls_private_key" "SSHKey" {
 
 
 resource "aws_key_pair" "KeyPair" {
-  key_name   = "${var.app_name}-TerraformKey"
+  key_name   = "${var.common_tags["app_name"]}-TerraformKey"
   public_key = tls_private_key.SSHKey.public_key_openssh
-  tags       = merge(var.common_tags, { Name = "${var.common_tags["app_name"}-${var.common_tags["env"]}-Key", CreationTime = local.tstmp})
+  tags       = merge(var.common_tags, { Name = "${var.common_tags["app_name"]}-${var.common_tags["env"]}-Key", CreationTime = local.tstamp})
 }
 
 
