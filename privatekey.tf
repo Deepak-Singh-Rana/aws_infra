1bc5c010e69369446613c2e92490f60b11c46218
Deepak Rana <dsr4hsp@gmail.com>
2023-04-17 17:24:18 +1000

    modified:   privatekey.tf

--- privatekey.tf
+++ privatekey.tf
@@ -3,13 +3,18 @@ resource "tls_private_key" "SSHKey" {
   rsa_bits  = 4096
 }
 
+
 resource "aws_key_pair" "KeyPair" {
   key_name   = "${var.app_name}-TerraformKey"
   public_key = tls_private_key.SSHKey.public_key_openssh
-  tags = {
-    DateOfCreation = local.tstmp
-  }
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.app_name}-TerraformKey"
+    },
+  )
 }
+
 
 resource "local_file" "PrivateKey" {
   content         = tls_private_key.SSHKey.private_key_pem
