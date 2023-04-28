03109e691a1025253e97db3bc8bb3ca6ac85b685
Deepak Rana <dsr4hsp@gmail.com>
2023-04-28 11:47:45 +1000

    modified:   locals.tf 	modified:   prod.tfvars 	renamed:    sg.tf -> security_groups.tf

--- locals.tf
+++ locals.tf
@@ -2,27 +2,27 @@ locals {
   tstamp = formatdate("DD-MMM-YYYY:hh-mm", timestamp())
 }
 
-locals {
 # EC2 configuration
+locals {
 ec2_conf = [
   {
     "tier"        = "web",
     "inst_cnt"    = "1",
     "inst_type"   = "t2.nano",
     "subnets"     = module.vpc.public_subnets.*,
-    "vpc_sg_ids"  = [aws_security_group.appsg.id, aws_security_group.websg.name]
+    "vpc_sg_ids"  = [${websgs}.name, ${appsgs}.name]
   },
   {
     "tier"        = "app",
     "inst_type"   = "t2.micro",
     "inst_cnt"    = "2"
     "subnets"     = module.vpc.private_subnets.*,
-    "vpc_sg_ids"  = [aws_security_group.appsg.id]
+    "vpc_sg_ids"  = [${appsgs}.name]
   }
  ]
 }
 
-
+# Loop to create EC2 configuration for multiple instances of different specifications
 locals {
   ser_conf = [
     for srv in local.ec2_conf : [
@@ -37,6 +37,7 @@ locals {
     ]
   ]
 }
+
 
 locals {
   instances = flatten(local.ser_conf)
--- prod.tfvars
+++ prod.tfvars
@@ -1,12 +1,21 @@
 primary_region    = "ap-southeast-2"
 zone              = "prod"
-cidr              = "10.0.0.0/16"
-private_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-public_subnets    = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+# VPC network
+vpc_cidr          = "10.1.0.0/16"
+private_subnets   = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+public_subnets    = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+
+# Security group and ports
+websgs            = ["aws_security_group.websg"]
+appsgs            = ["aws_security_group.appsg"]                    
+appsg_ports       = ["7015", "7025"]
+websg_ports       = ["8081", "9091"]
 
 
+# Common tags
 common_tags = {
-  "env"      = "prd"
-  "domain"   = "IT"
-  "app_name" = "BillApp"
+   "env"      = "prd"
+   "domain"   = "IT"
+   "app_name" = "BillApp"
 }
